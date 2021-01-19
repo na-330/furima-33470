b2c1b2f661bc6705b1a83dcee0ac3b10cc6433f5
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-19 18:55:50 +0900

    修正-1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,12 +85,12 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Price can't be blank")
         end
         it "価格が300以下では登録できない" do
-          @item.price = 100
+          @item.price = 299
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
         end
         it "価格が9999999以上では登録できない" do
-          @item.price = 99999999
+          @item.price = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
         end
