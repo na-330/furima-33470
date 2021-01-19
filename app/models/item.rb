0367469646970a75e66c857af836fca1ae902c9a
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-19 19:33:40 +0900

    修正-2

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,6 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :scheduled_delivery_id
     validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
-    validates :user
   end
 
 end
