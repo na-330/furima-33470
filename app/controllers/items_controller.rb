b4f99f3a211fa7c573d7d9403f7dd550ed35f118
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-19 09:18:17 +0900

    ルート・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @items  = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:info,:category_id,:delivery_fee_id,:prefecture_id,:product_status_id,:scheduled_delivery_id, :numericality, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,5 @@ class Item < ApplicationRecord
   belongs_to :product_status
   belongs_to :scheduled_delivery
 
-  validates :category_id,:delivery_fee_id,:prefecture_id,:product_status_id,:scheduled_delivery_id, numericality: { other_than: 1 } 
+  validates :category_id,:delivery_fee_id,:prefecture_id,:product_status_id,:scheduled_delivery_id, :numericality { other_than: 1 } 
 end
