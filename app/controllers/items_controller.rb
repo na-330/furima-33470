4b56b8b1981d4fb6e0b130a0ece6929d5b2811f6
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-20 14:59:50 +0900

    コントローラー設定（edit)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,10 +22,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    move_to_index(@prototype)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :delivery_fee_id, :prefecture_id, :product_status_id,
                                  :scheduled_delivery_id, :numericality, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index(item)
+    unless current_user.id == item.user.id
+      redirect_to root_path
+    end
+  end
+
 end
