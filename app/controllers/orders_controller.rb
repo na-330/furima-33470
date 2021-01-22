ba990407016546890f14730932ea3d313a8ab774
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-22 18:37:21 +0900

    ヴューの変更（solout or not)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :set_item, only: [:index, :create, :move_to_index]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @purchase = Purchase.new
