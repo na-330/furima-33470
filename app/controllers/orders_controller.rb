517a72d022aaf8586ce79be891a2da12ce14caf9
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-23 17:30:07 +0900

    クレカ機能設定（保存されるの確認）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
   def create
     @purchase = Purchase.new(order_params)
     if @purchase.valid?
-      # pay_item
+      pay_item
       @purchase.save
       redirect_to root_path
     else
@@ -20,17 +20,18 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:purchase).permit(:postal_code, :city, :house_number, :building, :phone_number, :prefecture_id, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase).permit(:postal_code, :city, :house_number, :building, :phone_number, :prefecture_id, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-  # , token: params[:token]
-  # def pay_item
-  #   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  #   Payjp::Charge.create(
-  #     amount: @item.price,
-  #     card: order_params[:token],
-  #     currency:'jpy'
-  #   )
-  # end
+
+  def pay_item
+    Payjp.api_key = "sk_test_e7f8fb15e5cd6e15f94e1a5e" # PAY.JPテスト秘密鍵
+    binding.pry
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
   
 
   def set_item
