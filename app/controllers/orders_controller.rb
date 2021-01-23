96fe6df829f4a1b27a194a57fcc2cf240d1a770a
na-330 <76425560+na-330@users.noreply.github.com>
2021-01-23 22:28:12 +0900

    修正-order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
   before_action :move_to_index, only: :index
 
@@ -34,9 +34,7 @@ class OrdersController < ApplicationController
   end
   
   def move_to_index
-    if current_user.id == @item.user_id
-      redirect_to root_path
-    elsif @item.order != nil
+    if current_user.id == @item.user_id || @item.order != nil
       redirect_to root_path
     end
   end
